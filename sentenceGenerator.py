c3b11e20c2d7d569c8427b9627a99c5ba8188014
slobo10 <97814567+slobo10@users.noreply.github.com>
2023-07-31 16:46:33 -0700

    Make basic sentence generator

---
+++ sentenceGenerator.py
@@ -0,0 +1,36 @@
+from random import choice
+
+words = {
+    'STARGATE': {
+        'names': ['Jack O\'neal', 'Danel Jackson', 'Samantha Carter', 'Teal\'c', 'Gorge Hammond', 'The shevron guy'],
+        'verbs': ['shot', 'searched', 'scanned', 'explored', 'blew up', 'talked to', 'ate', 'looked at', 'charted'],
+        'nouns': ['the gate', 'Earth', 'a death glider', 'the locals', 'the technology', 'the local food', 'a P-90'],
+    }
+}
+
+def generateSentence(wordSelection):
+    output = ''
+    output += choice(wordSelection['names']) + ' ' + choice(wordSelection['verbs']) + ' ' + choice(wordSelection['nouns']) + '.'
+    return(output)
+
+while True:
+    userInput = input('Select words: ').upper()
+
+    if userInput == 'END':
+        break
+    if userInput != '':
+        selectionExists = False
+        for i in words:
+            if userInput == i:
+                selectionExists = True
+                break
+        if selectionExists:
+            userSelection = userInput
+        else: 
+            print('Sorry! That isn\'t a word selection. Try one of these:')
+            for i in words:
+                print('\t' + i)
+            continue
+
+    print(generateSentence(words[userSelection.upper()]))
+    
